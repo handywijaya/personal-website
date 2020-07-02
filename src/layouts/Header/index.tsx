4731085dfc9c645f7dd139cbf3b3606ae1fdc0ed
Handy Wijaya <handy.wijaya.p@gmail.com>
2020-07-02 16:55:27 +0700

    revamp layout styling

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -6,10 +6,7 @@ class Header extends React.PureComponent {
         return (
             <div className="Header">
                 <div className="Header-title">
-                    Welcome! <span className="Header-snowflake">&#10052;</span>
-                </div>
-                <div className="Header-subtitle">
-                    This site is a handicraft of a developer who falls to one of Disney Movie.<br />Can you guess the movie? <span>&#129488;</span>
+                    Journey to Explore the World!
                 </div>
             </div>
         )
